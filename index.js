59708e9e39120f5ce37b804158fe09d5e84a5514
Sujinathakuri <sujchand2@gmail.com>
2021-04-15 09:38:36 +0545

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+
+
+updateEmployeeWithKeyAndValue()
+destructivelyUpdateEmployeeWithKeyAndValue()
+deleteFromEmployeeByKey()
+destructivelyDeleteFromEmployeeByKey()
+deleteFromEmployeeByKey()
